9e906b1bd4bad16c6d6335d6779f74bf5283dc3e
Ignacio-dev10 <ignacio.espinosamart@gmail.com>
2025-02-27 10:18:02 +0000

    mon super commit

--- TP2.R
+++ TP2.R
@@ -18,5 +18,66 @@ if(!dir.exists("fonds")) dir.create("fonds/", recursive = TRUE)
 
 download.file(url = url, destfile = paste0(file_path, file_name))
 
+## 1. Importation du fond
 
-## 1.
+carte <- st_read("fonds/commune_francemetro_2021.gpkg")
+
+
+## 2. Résumé descriptif
+str(carte)
+summary(carte)
+
+## 3. Regarder 10 premiers lignes et colonne
+
+View(carte[1:10,17])
+
+
+## 4. Affichage du système de projection
+
+st_crs(carte)
+
+## 5. Table uniquement avec communes bretonnes
+
+carte_bretagne <- carte %>%
+  filter(reg == 53) %>%
+  select(code, libelle, epc, dep, surf)
+
+## 6. Vérification classe
+
+class(carte_bretagne)
+
+
+## 7. Plot
+
+plot(carte_bretagne)
+
+## 8. Avec st_geometry
+
+plot(st_geometry(carte_bretagne), lwd=0.5)
+
+## 9. Surface
+
+carte_bretagne <- carte_bretagne %>%
+  mutate(surf2 = st_area(geom))
+
+## 10. Changement en km2
+
+str(carte_bretagne$surf2)
+
+# Technique pas fou : carte_bretagne$surf2 <- carte_bretagne$surf2/1000000
+
+carte_bretagne <- carte_bretagne %>%
+  mutate(surf2=units::set_units(surf2, km^2))
+
+
+## 11. Surf et surf2 égales ?
+
+# Non, jsp
+
+
+## 12. Surface des départements 
+
+dep_bretagne <- carte_bretagne %>%
+  group_by(dep) %>%
+  summarise(surface = sum(surf2))
+plot(dep_bretagne)

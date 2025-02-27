aae7533838924c068f225aef00beff1809302108
Ignacio-dev10 <ignacio.espinosamart@gmail.com>
2025-02-27 09:46:26 +0000

    good commit

--- TP2.R
+++ TP2.R
@@ -2,3 +2,21 @@
 
 ## Importation des packages
 
+require(dplyr)
+
+require(sf)
+
+#### Exercice 1 ####
+
+## 0. Importation des fonds
+
+url <- "https://minio.lab.sspcloud.fr/julienjamme/geographies/commune_francemetro_2021.gpkg"
+file_name <- "commune_francemetro_2021.gpkg"
+file_path <- "./fonds/"
+
+if(!dir.exists("fonds")) dir.create("fonds/", recursive = TRUE)
+
+download.file(url = url, destfile = paste0(file_path, file_name))
+
+
+## 1.
